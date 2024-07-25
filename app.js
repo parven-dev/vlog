9878f656a8346200c52631e14b1056a8faf36dba
trinity <ryan.cu1200@gmail.com>
2024-07-25 10:55:27 +0530

    run succesfully

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index", {foo:"FOO"});
+})
 
 
-const bodyParser = require("body-parser")
-
-app.use(bodyParser.urlencoded({ extended: True}))
+app.listen(port, function(){
+    console.log("running on port " + port)
+})
